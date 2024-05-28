219e248b330b87e0d13912604097f0ebe654edc0
nicolasAniort <nicolas.aniort@gmail.com>
2024-05-28 10:42:56 +0200

    first commit

---
+++ assets/modules/Home.js
@@ -0,0 +1,14 @@
+class Home{
+    constructor(){
+        console.log('HOME');
+            this.descriptionHTML = document.querySelector('.js-home-description');
+            this.profilHTML = document.querySelector('.js-home-profil');
+   
+    }
+    init(){
+        //Recuperer la description de mon profil Github
+        //Recuperer l'url de mon profil github
+    }
+}
+
+export {Home};
